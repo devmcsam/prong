72a2e2d317675933fddae327c45aa024d900e1a0
Samuel McNally <sammcnally587@gmail.com>
2026-08-21 16:04:40 -0500

    add incremental to speed up dev compile times, window spawning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 //! Rong is pong built with modern rust and the bevy game engine.
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
