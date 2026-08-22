334a8cec0d5c3efa9af1cbbd93299a78b548900c
Samuel McNally <sammcnally587@gmail.com>
2026-08-22 00:24:29 -0500

    spawning both paddles

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 //! Rong is pong built with modern rust and the bevy game engine.
 
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+const PADDLE_SIZE: Vec2 = Vec2::new(10.0, 100.0);
 
 fn main() {
     App::new()
@@ -9,6 +12,53 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
+#[derive(Component, Copy, Clone, PartialEq)]
+struct Paddle {
+    // only need to store y pos as they are on the edge of the window.
+    y_pos: f32,
+    speed: f32,
+}
+
+struct Ball {
+    position: Vec2,
+    direction: Vec2,
+    radius: f32,
+    speed: f32,
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Query<&Window, With<PrimaryWindow>>,
+) {
     commands.spawn(Camera2d);
+    let Ok(window) = window.single() else {
+        return;
+    };
+
+    let width = window.width();
+
+    let right_edge = width / 2.0;
+    let left_edge = -width / 2.0;
+
+    let mesh = meshes.add(Rectangle::new(PADDLE_SIZE.x, PADDLE_SIZE.y));
+    let material = materials.add(Color::srgb(0.5, 0.2, 0.8));
+
+    commands.spawn((
+        Mesh2d(mesh),
+        MeshMaterial2d(material),
+        Transform::from_xyz(right_edge - PADDLE_SIZE.x, 0.0, 0.0),
+        Paddle { y_pos: 0.0, speed: 50.0 }
+    ));
+
+    let mesh = meshes.add(Rectangle::new(PADDLE_SIZE.x, PADDLE_SIZE.y));
+    let material = materials.add(Color::srgb(0.5, 0.2, 0.8));
+
+    commands.spawn((
+        Mesh2d(mesh),
+        MeshMaterial2d(material),
+        Transform::from_xyz(left_edge + PADDLE_SIZE.x, 0.0, 0.0),
+        Paddle { y_pos: 0.0, speed: 50.0 }
+    ));
 }
